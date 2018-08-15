09b5658b59f1066656a1711ef3674f450709a36d
Murat Ozcan <muratkerem@gmail.com>
2018-08-15 01:55:27 -0500

    repeat

--- 3.8_default_parameters/Listing3.9.js
+++
@@ -1,8 +0,0 @@
-var assert = require('assert');
-
-function performAction(ninja, action = "skulking") {
-  return ninja + " " + action;
-
-}
-assert(performAction("Fuma") === "Fuma skulking", console.log("The default value is used for Fuma"));
-assert(performAction("Yagyu", "sneaking") === "Yagyu sneaking", console.log("Yagyu can do whatever he pleases, even sneak!"));
